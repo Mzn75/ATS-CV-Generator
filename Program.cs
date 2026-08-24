271f2c6a404028b3f3dcccbaf9d74152e3394934
Mzn75 <mazeenff7@gmail.com>
2026-08-24 21:49:46 +0300

    Solve an issue with the view

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using ATS_CV_Generator.Data;
+using ATS_CV_Generator.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,9 +11,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
