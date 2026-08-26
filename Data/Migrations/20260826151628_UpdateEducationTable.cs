6a28a1ffb5cb3c1101dd71430939dcfa43a24f91
Mzn75 <mazeenff7@gmail.com>
2026-08-26 18:17:54 +0300

    Edit education table

---
+++ Data/Migrations/20260826151628_UpdateEducationTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ATS_CV_Generator.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEducationTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StartDate",
+                table: "Educations",
+                newName: "Major");
+
+            migrationBuilder.RenameColumn(
+                name: "EndDate",
+                table: "Educations",
+                newName: "GradDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Major",
+                table: "Educations",
+                newName: "StartDate");
+
+            migrationBuilder.RenameColumn(
+                name: "GradDate",
+                table: "Educations",
+                newName: "EndDate");
+        }
+    }
+}
