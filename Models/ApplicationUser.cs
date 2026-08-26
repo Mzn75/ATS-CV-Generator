8bbb20e50f71ccbe8b1544cd50ca00825a7e750e
Mzn75 <mazeenff7@gmail.com>
2026-08-26 12:28:26 +0300

    Add Certificates table to the Db

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -14,5 +14,6 @@ namespace ATS_CV_Generator.Models
         public virtual ICollection<Experience> Experiences { get; set; } = new List<Experience>();
         public virtual ICollection<Education> Educations { get; set; } = new List<Education>();
         public virtual ICollection<ProjectItem> Projects { get; set; } = new List<ProjectItem>();
+        public virtual ICollection<Certificates> Certificates { get; set; } = new List<Certificates>();
     }
 }
---
+++ Models/Certificates.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ATS_CV_Generator.Models
+{
+    public class Certificates
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Certificate name is required")]
+        [Display(Name = "Certificate Name", Prompt = "e.g., Python Fundanmentals")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Issuing organization is required")]
+        [Display(Name = "Issuing Organization", Prompt = "e.g., Huawei")]
+        public string? Issuer { get; set; }
+
+        [Required(ErrorMessage = "Issue date is required")]
+        [Display(Name = "Date Earned", Prompt = "e.g., August 2026")]
+        public string? IssueDate { get; set; }
+
+        [Display(Name = "Credential URL", Prompt = "e.g., https://...")]
+        public string? CredentialUrl { get; set; }
+
+        // --- THE RELATIONAL LINK ---
+        [ForeignKey("CvDraft")]
+        public int CvDraftId { get; set; }
+
+        public CvDraft? CvDraft { get; set; }
+    }
+}
