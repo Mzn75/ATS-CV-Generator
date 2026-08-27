1a6ed9f75fa35d24126f938f7cded260e039e616
Mzn75 <mazeenff7@gmail.com>
2026-08-27 12:44:44 +0300

    Make fields required to avoid null errors

--- Models/Education.cs
+++ Models/Education.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace ATS_CV_Generator.Models
 {
@@ -9,10 +10,18 @@ namespace ATS_CV_Generator.Models
         public string UserId { get; set; } = string.Empty;
         public virtual ApplicationUser? User { get; set; }
 
+        [Required(ErrorMessage = "Please enter your degree.")]
         public string Degree { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter your major.")]
         public string Major { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter the institution.")]
         public string Institution { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please select your graduation date.")]
         public string GradDate { get; set; } = string.Empty;
+
         [ForeignKey("CvDraft")]
         public int CvDraftId { get; set; }
 
--- Models/Experience.cs
+++ Models/Experience.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace ATS_CV_Generator.Models
 {
@@ -9,11 +10,22 @@ namespace ATS_CV_Generator.Models
         public string UserId { get; set; } = string.Empty;
         public virtual ApplicationUser? User { get; set; }
 
+        [Required(ErrorMessage = "Please enter your job title.")]
         public string JobTitle { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter the company name.")]
         public string Company { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter the company location.")]
         public string Location { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please select job starting date.")]
         public string StartDate { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please select job ending date.")]
         public string EndDate { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter a brief description.")]
         public string Description { get; set; } = string.Empty;
         [ForeignKey("CvDraft")]
         public int CvDraftId { get; set; }
